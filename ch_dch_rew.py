a550dbaa38b0c2f1aa403ec2434c2361d12991e8
CristianColdea <rdt333@gmail.com>
2023-01-11 10:19:09 +0200

    ch_dch reward script update

--- ch_dch_rew.py
+++ ch_dch_rew.py
@@ -3,3 +3,12 @@ Script to compute the charge-discharge reward
 """
 
 import sys
+
+def reward(soc):
+    """
+    Function to compute charge/discharge reward value.
+    Takes as input the state of charge, in percent, and
+    returns a the reward value.
+    """
+
+    return soc
