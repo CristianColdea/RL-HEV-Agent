c9e319c3a4daf517435f68c48bcffa80016e3886
CristianColdea <rdt333@gmail.com>
2023-03-19 19:51:36 +0200

    charge/discharge reward update

--- ch_dch_rew.py
+++ ch_dch_rew.py
@@ -4,11 +4,10 @@ Script to compute the charge-discharge reward
 
 import sys
 
-def reward(soc, prev_rew, rew_up_lim = 5, rew_low_lim = -1000, soc_up =80, soc_low = 40):
+def reward(soc, rew_up_lim = 5, rew_low_lim = -1000, soc_up =80, soc_low = 40):
     """
     Function to compute charge/discharge reward value.
     Takes as input the state of charge (SoC), in percent,
-    previous charge reward value,
     upper and lower limits of the reward,
     upper and lower limits of the SoC, and
     returns the SoC and depth of discharge (DoC) reward values.
