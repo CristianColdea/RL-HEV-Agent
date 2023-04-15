f355b2201c6545850bee450cff424397561f50bc
CristianColdea <rdt333@gmail.com>
2023-04-15 09:18:45 +0300

    first commit of a script intended for power flows efficiency

---
+++ eta_flows.py
@@ -0,0 +1,7 @@
+"""
+Script to compute each elementary power flow efficiency.
+For further details please see flows_efficiencies_rewards.md file
+"""
+
+def flow1(pinput, eta_trans=0.95):
+    return
