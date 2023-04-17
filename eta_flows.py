b2dbef0fd359e90e88590a3b8e62d145bfcb6695
CristianColdea <rdt333@gmail.com>
2023-04-17 11:36:28 +0300

    script update

--- eta_flows.py
+++ eta_flows.py
@@ -17,10 +17,19 @@ def flow1(pinput, sfc, eta_trans = 0.95, q_i = 41.84):
     
     return (pinput * eta_ice * eta_trans), (eta_ice * eta_trans)
 
-def flow2(pinput, eta_dch, eta_inv, eta_trans, eta_EM):
+def flow2(pinput, eta_dch=0.89, eta_inv=0.9, eta_trans=0.98, eta_EM):
     """
     Function to compute overall efficiency from battery to the wheels,
     via EM.
+    Takes as inputs the input power, in kW, the discharge efficiency,
+    the inverter unit overall efficiency, EM gear ratio efficiency,
+    and the EM efficiency.
+    Returns a tuple with the delivered power
+    and the efficiency of the power flow.
     """
 
+    return (pinput * eta_dch * eta_inv * eta_trans * eta_EM),\
+           (eta_dch * eta_inv * eta_trans * eta_EM)
+
+def flow3(pinput, eta_trans=0.985, eta_EG, eta_rectif=0.92, eta_ch=0.88):
     return
