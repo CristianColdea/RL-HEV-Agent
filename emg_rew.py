aa8da2d1e56d5bc473f4326806ae44a7c603348e
CristianColdea <rdt333@gmail.com>
2023-04-29 20:36:57 +0300

    first commit

---
+++ emg_rew.py
@@ -0,0 +1,11 @@
+#script to shape the Electric Motor and Generator reward
+
+
+def(eta):
+    """
+    The function to compute reward value.
+    Takes as input the electric machine efficiency.
+    Returns the reward value.
+    """
+
+    return 0
