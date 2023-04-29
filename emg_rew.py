50d51252310d89b3e565a4bb8f503d9a3034e6e1
CristianColdea <rdt333@gmail.com>
2023-04-29 20:43:11 +0300

    script update

--- emg_rew.py
+++ emg_rew.py
@@ -1,11 +1,16 @@
-#script to shape the Electric Motor and Generator reward
+"""
+Script to shape the Electric Motor and Generator reward
+"""
 
 
-def(eta):
+def emg_rew(eta):
     """
     The function to compute reward value.
     Takes as input the electric machine efficiency.
     Returns the reward value.
     """
 
-    return 0
+    return (-750 * eta**2 + 1625 * eta - 770)
+
+# test the function
+# print(emg_rew(0.96))
