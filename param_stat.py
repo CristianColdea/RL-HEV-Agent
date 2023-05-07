4e5d6fe53a5c8edda69248688bb720c958be357b
CristianColdea <rdt333@gmail.com>
2023-05-07 10:48:32 +0300

    script update

--- param_stat.py
+++ param_stat.py
@@ -6,3 +6,7 @@ per timestep.
 import simfc3.py as sfc
 
 # Class to determine the whole bunch of parameters of interest
+
+class Status:
+    def __init__(self, v_init, a, tmstp, soc):
+        self.v_init = v_init
