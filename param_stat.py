2a4ed3b5b459b44c1469350fdb457e55713d363d
CristianColdea <rdt333@gmail.com>
2023-05-07 10:44:54 +0300

    first commit

---
+++ param_stat.py
@@ -0,0 +1,8 @@
+"""
+Script to determine the current powertrain status parameters of interest,
+per timestep.
+"""
+
+import simfc3.py as sfc
+
+# Class to determine the whole bunch of parameters of interest
