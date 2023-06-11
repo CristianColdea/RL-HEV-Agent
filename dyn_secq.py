9a0ecdb45766c08cc590e024b72e681b9fca28d9
CristianColdea <rdt333@gmail.com>
2023-06-11 15:52:20 +0300

    code update

--- dyn_secq.py
+++ dyn_secq.py
@@ -3,7 +3,7 @@ This is the script for storing the needed secquences of dynamically
 changing variables, i.e. initial speed, gear ratio, acceleration,
 and time of accelerated movement.
 The script will be read subsequently by the RL agent, the mentioned
-values being used to compute specific fuel/enegy consumption.
+values being used to compute specific fuel/energy consumption.
 """
 
 """
@@ -14,7 +14,7 @@ acceleration and time.
 
 dyns = []   #list for storing tuples
 
-# first tuple: gear ratio, initial null speed,
+# first tuple: initial null speed, gear ratio,
 # acceleration 1.2 m/s**2 applied for 12 seconds
 
 a = 0, 0.94, 1.2, 12
