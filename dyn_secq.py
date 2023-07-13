9992a2dde91da3af9bcc8e66a189f38dc3bd8ed6
CristianColdea <rdt333@gmail.com>
2023-07-13 22:23:01 +0300

    code update

--- dyn_secq.py
+++ dyn_secq.py
@@ -7,21 +7,21 @@ values being used to compute specific fuel/energy consumption.
 """
 
 """
-the changing values are stored in a list of tuples,
-in the following order: initial speed, gear ratio,
+the changing values are stored in a list of lists,
+in the following order: initial speed, gearbox ratio,
 acceleration and time.
 """
 
-dyns = []   #list for storing tuples
+dyns = []   #list for storing lists
 
-# first tuple: initial null speed, gear ratio,
+# first list: initial null speed, gearbox ratio,
 # acceleration 1.2 m/s**2 applied for 12 seconds
 
-a = 0, 0.94, 1.2, 12
-dyns.append(a)   #append the tuple
-b = 3, 2.94, 0.3, 12
+a = [0, 0.94, 1.2, 12]
+dyns.append(a)   #append the list
+b = [3, 2.94, 0.3, 12]    #second list
 dyns.append(b)
 
-#for secq in dyns:
-#    for item in secq:
-#        print(item)
+for secq in dyns:
+    for item in secq:
+        print(item)
