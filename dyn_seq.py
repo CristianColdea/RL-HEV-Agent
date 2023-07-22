d921fc5d3eed5cb289ae7521e1b33bbf2074f1df
CristianColdea <rdt333@gmail.com>
2023-07-22 15:59:54 +0300

    code update

--- dyn_seq.py
+++ dyn_seq.py
@@ -7,13 +7,26 @@ values being used to compute specific fuel/energy consumption.
 """
 
 """
-the changing values are stored in a list of lists,
+The changing values are stored in a final list of lists,
 in the following order: initial speed, gearbox ratio,
 acceleration and time.
+_____
+The initial list of lists, as value resulted from the WLTP cycle speed
+profile, is introduced.
+The values are intial speed, final speed, initial time, final time,
+precisely in this order.
 """
 
-dyns = []   #list for storing lists
-
+# initial list of list (list of sequences) for WLTP cycle low speed section
+low_row = [[0, 45, 10, 30], [45, 12,30, 55], [12, 40, 55, 75],
+           [40, 0, 75, 100], [0, 30, 140, 150], [30, 12, 150, 155],
+           [12, 38, 155, 170], [38, 25, 170, 175], [25, 30, 175, 180],
+           [30, 12, 180, 200], [12, 57, 200, 225], [57, 14, 225, 255],
+           [14, 52, 255, 275], [52, 20, 275, 285], [20, 50, 285, 295],
+           [50, 46, 295, 300], [46, 13, 300, 340], [13, 27, 340, 350],
+           [27, 20, 350, 365], [20, 25, 365, 370], [25, 0, 370, 385],
+           [0, 30, 385, 400], [30, 13, 400, 415], [13, 33, 415, 435],
+           ]
 # first list: initial null speed, gearbox ratio,
 # acceleration 1.2 m/s**2 applied for 12 seconds
 
