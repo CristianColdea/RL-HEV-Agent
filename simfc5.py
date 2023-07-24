7f790db37574c994f162f0c6a3809c50b36180f3
CristianColdea <rdt333@gmail.com>
2023-07-24 15:14:35 +0300

    code update

--- simfc5.py
+++ simfc5.py
@@ -79,7 +79,6 @@ class Mus:
             return outS
         else:
             outC = P_max * ((n_i/n_max) + 0.5 * (n_i/n_max)**2 - 0.5 * (n_i/n_max)**3)
-            print("The CIE output for the given speed is: ", outC)
             return outC
 
     # mu_n function for continuous generation of muN
