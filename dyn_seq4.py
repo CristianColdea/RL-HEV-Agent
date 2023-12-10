2d54dcda6107abc8e4957865333e38cb1eab12e6
CristianColdea <rdt333@gmail.com>
2023-12-10 18:45:27 +0200

    code update

--- dyn_seq4.py
+++ dyn_seq4.py
@@ -252,14 +252,14 @@ def process_input(processed, gear_ini, min_lim=1800, max_lim=3100, tstep=0.5,
                                        sc.xi_gs[posi-1], dict_fix['r_d'],
                                        dict_fix['s_f'], dict_fix['n_max'])
                 
-                # increase speed by timestep, within engine speed limits
-                while (n_next <= min_lim):
+                # decrease speed by timestep, within engine speed limits
+                while (n_next >= max_lim):
                     # print("processed inside while, ", processed)
                     processed[0] = processed[0] + tstep * processed[2]
                     processed[1] = gear
                     processed[3] = tstep
 
-                    if (processed[0] >= v_ref):   # reached the end of sequence
+                    if (processed[0] <= v_ref):   # reached the end of sequence
                         processed[0] = v_ref
                         if (t_init % tstep > 0):
                             processed[3] = t_init % tstep
@@ -271,17 +271,17 @@ def process_input(processed, gear_ini, min_lim=1800, max_lim=3100, tstep=0.5,
                     # print("n_next as control variable not in the last gear, ", n_next)
                     # print("processed[0], ", processed[0])
                     n_next = sfc.engine_speed(processed[0], dict_fix['xi_f'],
-                                       sc.xi_gs[posi+1], dict_fix['r_d'],
+                                       sc.xi_gs[posi-1], dict_fix['r_d'],
                                        dict_fix['s_f'], dict_fix['n_max'])
 
             
-            else:   # in the last gear
+            else:   # in the first gear
                 # current engine speed
                 n_i = sfc.engine_speed(processed[0], dict_fix['xi_f'],
                                        gear, dict_fix['r_d'],
                                        dict_fix['s_f'], dict_fix['n_max'])
 
-                # increase speed by timestep up to MAX limit
+                # decrease speed by timestep down to stable speed limit
                 while (n_i <= n_max):
                     processed[0] = processed[0] + tstep * processed[2]
                     processed[1] = gear
