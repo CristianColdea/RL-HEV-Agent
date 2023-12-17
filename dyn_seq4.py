20293bd435194931f75be79f6cd849e7476568b2
CristianColdea <rdt333@gmail.com>
2023-12-17 08:22:35 +0200

    code update

--- dyn_seq4.py
+++ dyn_seq4.py
@@ -327,12 +327,12 @@ for sequence in low_raw[:2]:
     # if expand empty
     if not expand:
         expand.extend(process_input(raw_proc(sequence), sequence[1]))
-        print("expand[0], ", expand)
+        print(expand)
     else:   # use the previous sequence gear
     #    expand.extend(process_input(raw_proc(sequence),
     #                                         expand[-1][1]))
         print("Seq 2, ", sequence)
 
-#print(process_input(raw_proc(low_raw[0]), low_raw[0][1]))
+print(process_input(raw_proc(low_raw[0]), low_raw[0][1]))
 #print("**********")
 # print(expand)
