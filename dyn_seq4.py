99505b00cf35ff3bedc85c71b9dce6bcfde750f7
CristianColdea <rdt333@gmail.com>
2024-01-31 20:26:18 +0200

    script update

--- dyn_seq4.py
+++ dyn_seq4.py
@@ -418,8 +418,8 @@ for sequence in low_raw:
         # print(expand)
         # print("**********")
 
-# print(expand)
-print(len(expand))
+print(expand)
+# print(len(expand))
 
 #print(process_input(raw_proc(low_raw[0]), low_raw[0][1]))
 # print(process_input(raw_proc(low_raw[1]), 1.211))
