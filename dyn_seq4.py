57ffe98de03df97dedff4f821d245ece1214c71b
CristianColdea <rdt333@gmail.com>
2024-02-15 21:03:14 +0200

    script update

--- dyn_seq4.py
+++ dyn_seq4.py
@@ -568,7 +568,7 @@ with open('wltp_full.csv', 'w', newline='') as file:
 
 # list to store results of low speed section
 fuels_low = ["l/100", "kg/100", "kg/h", "sfc"]
-
+"""
 for seq in expand_low[1:]:
     # deccelerated movement starting with null initial speed not possible
     if seq[0] == 0 and seq[2] < 0:
@@ -585,11 +585,11 @@ with open('fcons_low.csv', 'w', newline='') as file:
     writer = csv.writer(file)
 
     writer.writerows(fuels_low)
-
+"""
 # list to store results of medium speed section
 fuels_med = ["l/100", "kg/100", "kg/h", "sfc"]
 
-for seq in expand_med[1:]:
+for seq in expand_med[90:110]:
     # deccelerated movement starting with null initial speed not possible
     if seq[0] == 0 and seq[2] < 0:
         continue
