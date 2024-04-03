be1958a844c15c538434e01b593eb6781339be41
CristianColdea <rdt333@gmail.com>
2024-04-03 07:11:23 +0300

    input data update

--- work_kE.py
+++ work_kE.py
@@ -97,40 +97,15 @@ def wk_avmus(m, v_init, a, t, gamma_m=1.08, eta_t=0.95, eta_max=0.4,
 
     return wk_avmus
 
-def wk_parts(m, v_init, a, t, gamma_m=1.08, eta_t=0.98, eta_max=0.4,
-               mu_init=0.81, mu_fin=0.83):
-    """
-    Function to compute the work of the inertial force as weighted components
-    during accelerated period.
-    Takes as inputs the vehicle mass, initial speed, acceleration, time,
-    coefficient of rotational masses, vehicle transmission efficiency,
-    engine max. efficiency, engine initial efficiency coefficient and
-    engine final efficiency coefficient.
-    Returns the kinetic energy variation.
-    Note:  engine efficiency coefficients across speed and output
-    ranges are taken into account.
-    """
-
-    # same coefficient for facile formula writting
-    C4 = (m * gamma_m * a * t) / (eta_t * eta_max)
-
-    wk_parts_init = (C4 * v_init * t) / mu_init
-    print(wk_parts_init)
-    wk_parts_accel = (C4 * a * t) / (2 * mu_fin)
-    print(wk_parts_accel)
-
-    return (wk_parts_init + wk_parts_accel)
-
-
 # The data to be used to check scenarios
 m = 2235
 a = 0.38889
 t = 1
-v_init = 2
+v_init = 3.72222
 eta_max = 0.4
 eta_t = 0.95
-mu_init = 0.85
-mu_fin = 0.78
+mu_init = 0.544684
+mu_fin = 0.363850
 
 
 print("Kinetic energy variation, no mus, ", kE(m, v_init, a, t))
